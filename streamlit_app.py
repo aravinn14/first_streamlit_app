227415970df3a4d2240561278c1827dece9f1eaa
aravinn14 <123214382+aravinn14@users.noreply.github.com>
2023-01-21 14:13:19 -0500

    Update streamlit_app.py
    Added smootie option header

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,5 @@ streamlit.text('Omega 3 and Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 streamlit.text('Avacado Toast')
+
+streamlit.header('Build Your Own Fruit Smoothie')
